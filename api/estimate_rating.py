1b6aa2a3b405291301fc71bd79d0a4ec5c788795
Angela Popa <angela.popa@student.uibk.ac.at>
2020-09-29 18:54:58 +0200

    fix CORS issues

--- api/estimate_rating.py
+++ api/estimate_rating.py
@@ -1,13 +1,19 @@
 import flask
 from flask import request, jsonify
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+# with default arguments in order to allow CORS for all domains on all routes
+# CORS(app)
+cors = CORS(app, resources={
+            r"/api/*": {"origins": {"https://epc-modelling-app.herokuapp.com/", "http://localhost:3000"}}})
 
-@app.route('/api/estimate', methods=['GET'])
+
+@ app.route('/api/estimate', methods=['GET'])
 def api_estimate():
-
+    print("we entered the estimate method.")
     # Check if floor_area and total_energy were provided as part of the URL.
     if 'floor_area' in request.args:
         floor_area = int(request.args['floor_area'])
@@ -22,15 +28,16 @@ def api_estimate():
     # some test data
     estimated_data = [
         {'id': 0,
-         'Rating': 'C',
+         'rating': 'C',
          'neighbours': 'no neighbours yet',
          'input_floor_area': floor_area,
-         'input_total_energy': total_energy}
+         'input_total_energy': total_energy}, {'id': 1, 'somestuff': 5}
     ]
-
+    print(estimated_data)
     return jsonify(estimated_data)
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def page_not_found(e):
+    print("we entered page not found")
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from api.estimate_rating import app
 
 if __name__ == "__main__":
+    print("we are in wsgi")
     app.run()
